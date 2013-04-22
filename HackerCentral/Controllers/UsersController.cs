0ef3c80dd264a46c742690e2ff3a118c219823f8
Ryan Erdmann <ryanerdmann@hotmail.com>
2013-04-22 00:00:03 -0500

    Add View Users interface to Admin Mode

---
+++ HackerCentral/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using HackerCentral.Accessors;
+using HackerCentral.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackerCentral.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+        public ActionResult Index()
+        {
+            var ua = new UserAccessor();
+            var m = new UsersViewModel()
+            {
+                Users = ua.GetAllUsers()
+            };
+
+            return View("Index", m);
+        }
+
+    }
+}
---
+++ HackerCentral/ViewModels/UsersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HackerCentral.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackerCentral.ViewModels
+{
+    public class UsersViewModel
+    {
+        public List<User> Users { get; set; }
+
+        public UsersViewModel()
+        {
+        }
+    }
+}
