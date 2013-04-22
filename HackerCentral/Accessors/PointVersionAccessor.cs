6f25ae925d61baa7d47a58181d02e589b630899a
Ryan Erdmann <ryanerdmann@hotmail.com>
2013-04-22 01:06:51 -0500

    Implemented PointVersion API

---
+++ HackerCentral/Accessors/PointVersionAccessor.cs
@@ -0,0 +1,70 @@
+﻿using HackerCentral.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace HackerCentral.Accessors
+{
+    public class PointVersionAccessor
+    {
+        private int conversationId;
+        private string apiKey;
+        private int userId;
+
+        public PointVersionAccessor()
+        {
+            conversationId = AthenaBridgeAPISettings.CONVERSATION_ID;
+            apiKey = AthenaBridgeAPISettings.API_KEY;
+            userId = AthenaBridgeAPISettings.USER_ID;
+        }
+
+        public List<PointVersion> GetAllPointVersions()
+        {
+            string api_url = String.Format("http://athenabridge.com/api/{0}/{1}/point_versions", apiKey, conversationId);
+
+            using (var w = new WebClient())
+            {
+                try
+                {
+                    var jsonData = string.Empty;
+                    jsonData = w.DownloadString(api_url);
+                    List<PointVersion> versions = JsonConvert.DeserializeObject<List<PointVersion>>(jsonData);
+                    if (versions.Count > 0)
+                        return versions;
+                    else
+                        return new List<PointVersion>();
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public List<PointVersion> GetPointVersionsByPoint(long pointId)
+        {
+            string api_url = String.Format("http://athenabridge.com/api/{0}/{1}/point_versions/point/{2}", apiKey, conversationId, pointId);
+
+            using (var w = new WebClient())
+            {
+                try
+                {
+                    var jsonData = string.Empty;
+                    jsonData = w.DownloadString(api_url);
+                    List<PointVersion> versions = JsonConvert.DeserializeObject<List<PointVersion>>(jsonData);
+                    if (versions.Count > 0)
+                        return versions;
+                    else
+                        return new List<PointVersion>();
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ HackerCentral/Models/PointVersion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HackerCentral.Models
+{
+    public class PointVersion
+    {
+        public Category? category { get; set; }
+        public bool? collaboratable { get; set; }
+        public long? conversation_depth { get; set; }
+        public long? conversation_id { get; set; }
+        public long? counterpoint_count { get; set; }
+        public DateTime? created_at { get; set; }
+        public bool? editable { get; set; }
+        public string full_text { get; set; }
+        public long? id { get; set; }
+        public long? initial_statement_id { get; set; }
+        public bool? is_hidden { get; set; }
+        public long? parent_id { get; set; }
+        public long? point_id { get; set; }
+        public long? point_version { get; set; }
+        public double? quality { get; set; }
+        public long? relevance_ratings_count { get; set; }
+        public double? relevance_score { get; set; }
+        public long? response_count { get; set; }
+        public Side? side { get; set; }
+        public bool? spam { get; set; }
+        public bool? spam_child { get; set; }
+        public string summary { get; set; }
+        public DateTime? updated_at { get; set; }
+        public long? user_id { get; set; }
+        public long? validity_ratings_count { get; set; }
+        public double? validity_score { get; set; }
+        public long? views { get; set; }
+
+        public enum Side { pro, con };
+        public enum Category
+        {
+            [Description("Supporting")]
+            SUPPORTING = 1,
+            [Description("Opposing")]
+            OPPOSING = 2
+        }
+    }
+}
