7b7171921d0a8675692fc005b42530711ea0f13a
Kevin North <knorth@huskers.unl.edu>
2013-09-16 17:09:07 -0500

    Working on a page to view info stored in tracking system. (Doesn't work yet, but doesn't break build.)

---
+++ HackerCentral/Controllers/TrackingController.cs
@@ -0,0 +1,49 @@
+﻿using HackerCentral.Models;
+using HackerCentral.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackerCentral.Controllers
+{
+    /// <summary>
+    /// Show information stored by the tracking system
+    /// </summary>
+    public class TrackingController : TrackedController
+    {
+        [HttpGet]
+        public ActionResult Data(bool isLimited = true, int numToShow = 10)
+        {
+            using(SimpleContext dbContext = new HackerCentralContext(this))
+            {
+                List<ActionTrack> actionTracks;
+
+                if(isLimited)
+                {
+                    actionTracks = dbContext.ActionTracks
+                        .OrderByDescending(a => a.TimeStamp)
+                        .Take(numToShow).ToList();
+                }
+                else
+                {
+                    actionTracks = dbContext.ActionTracks
+                        .OrderByDescending(a => a.TimeStamp)
+                        .ToList();
+                }
+
+                var model = new TrackingViewModel()
+                {
+                    IsLimited = isLimited,
+                    NumActionsShown = actionTracks.Count(),
+                    ActionTracks = actionTracks
+                };
+
+                return View(model);
+            }
+        }
+
+    }
+}
---
+++ HackerCentral/ViewModels/TrackingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using HackerCentral.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackerCentral.ViewModels
+{
+    public class TrackingViewModel
+    {
+        public List<ActionTrack> ActionTracks;
+        public bool IsLimited;
+        public int NumActionsShown;
+    }
+}
